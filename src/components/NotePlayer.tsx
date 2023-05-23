7755603967021b1fbbfa03fe699a19b24c7363cb
Ethan Yu <eykyudev@gmail.com>
2023-05-23 15:26:07 -0700

    attempt at vercel fix

--- src/components/NotePlayer.tsx
+++ src/components/NotePlayer.tsx
@@ -13,7 +13,7 @@ function NotePlayer(props: NotePlayerProps) {
       <h1>please select <br></br>the correct note</h1>
       <p>you can replay the sound<br></br> by clicking the play button below.</p>
       <button className='playButton'>
-        <img src="src/images/play.png" alt="Play" />
+        <img src="public/images/play.png" alt="Play" />
       </button>
       <div className="noteSelector">
         {noteList.map((note) => (
